f6060dd1cc41dcb2b1a1663cf86fc46a839192fe
ttllyy <qy_dlut@163.com>
2017-07-22 01:55:50 +0800

    modify function name BP

--- mnist/train_cnn.py
+++ mnist/train_cnn.py
@@ -9,6 +9,7 @@ class MnistCNNTest(object):
 
 
     def TrainCNN(self):
+        #test accuracy 0.9815
         def weight_variable(shape):
           initial = tf.truncated_normal(shape, stddev=0.1)
           return tf.Variable(initial)
@@ -74,7 +75,8 @@ class MnistCNNTest(object):
 
 
 
-    def TrainNN(self):
+    def TrainBP(self):
+        #test accuracy 0.9808
         def weight_variable(shape):
           initial = tf.truncated_normal(shape, stddev=0.1)
           return tf.Variable(initial)
@@ -134,12 +136,57 @@ class MnistCNNTest(object):
             print("test accuracy %g" % accuracy.eval(feed_dict={x: self.mnist.test.images, y_: self.mnist.test.labels, keep_prob: 1.0}))
 
 
+
+    def TrainBPAndNoDrop(self):
+        #test accuracy 0.9817
+        def weight_variable(shape):
+          initial = tf.truncated_normal(shape, stddev=0.1)
+          return tf.Variable(initial)
+
+        def bias_variable(shape):
+          initial = tf.constant(0.1, shape=shape)
+          return tf.Variable(initial)
+
+        x = tf.placeholder(tf.float32, shape=[None, 784])
+        y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+        W_conv1 = weight_variable([784, 1024])
+        b_conv1 = bias_variable([1024])
+        h_conv1 = tf.nn.relu(tf.matmul(x, W_conv1) + b_conv1)
+
+
+        W_conv2 = weight_variable([1024, 1024])
+        b_conv2 = bias_variable([1024])
+        h_conv2 = tf.nn.relu(tf.matmul(h_conv1, W_conv2) + b_conv2)
+
+        W_fc2 = weight_variable([1024, 10])
+        b_fc2 = bias_variable([10])
+
+        y = tf.nn.softmax(tf.matmul(h_conv2, W_fc2) + b_fc2)
+        cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(20000):
+                batch = self.mnist.train.next_batch(50)
+                if i % 100 == 0:
+                    train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1]})
+                    print("step %d, training accuracy %g" % (i, train_accuracy))
+                train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+            print("test accuracy %g" % accuracy.eval(feed_dict={x: self.mnist.test.images, y_: self.mnist.test.labels}))
+
+
 if __name__ == "__main__":
     print("test begin...")
 
     testInst = MnistCNNTest()
-    testInst.TrainNN()
-    # testInst.TrainCNN()
+    # testInst.TrainNN()
+    # testInst.TrainNNAndNoDrop()
+    testInst.TrainCNN()
 
     print("test end...")
 
